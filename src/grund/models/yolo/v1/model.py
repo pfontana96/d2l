fix iou computation to contemplate broadcasting

src/grund/models/yolo/v1/model.py
@@ -141,9 +141,10 @@ class YOLOv1(nn.Module):
     ) -> torch.Tensor:
         """
         Compute the loss for YOLOv1 predictions.
-        :param predictions: Model predictions
-        :param targets: Ground truth targets
-        :return: Computed loss value
+        
+        Args:
+        y_pred (torch.Tensor): Tensor of predictions with shape (7, 7, B * 5 + C)
+        y_true (torch.Tensor): Tensor of targets with shape (7, 7, B * 5 + C)
         """
         assert y_pred.shape == y_true.shape == (number_of_gridcells, number_of_gridcells, 5*boxes_per_cell + number_of_classes)
         # As stated in the paper:
@@ -158,12 +159,20 @@ class YOLOv1(nn.Module):
         object_presence_mask = y_true[..., 4] > 0
         object_absence_mask = ~object_presence_mask
 
+        # Assume that gt box is first one
+        gt_bboxes = y_true[object_presence_mask][:5]
+
+        # compute IOU for predictions to find responsible predictor
+        # TODO: MOdif iou so that it can broadcast, as we have N,1 gt boxes (one per each image with a detection)
+        # but N, B predictor boxes and we need to find the responsable predictor box for each n in N
+        pred_bboxes_xyxy = xywh2xyxy(y_pred[object_presence_mask].reshape(-1, 5), gt)
+
         # X and Y loss
-        xy_loss = ((y_true[object_presence_mask][:2] - y_pred[object_presence_mask][:2]) ** 2).sum(dim=-1).sum()
+        xy_loss = ((y_true[object_presence_mask][:2] - y_pred[object_presence_mask][:2]) ** 2).sum()
 
         # Width and Height loss
         eps = 1e-6
-        wh_loss = (torch.sqrt(y_true[object_presence_mask][2:] - y_pred[object_presence_mask][2:]) ** 2).sum(dim=-1).sum()
+        wh_loss = (torch.sqrt(y_true[object_presence_mask][2:] - y_pred[object_presence_mask][2:]) ** 2).sum()
 
         # Confidence loss
         confidence_loss = ((y_true[object_presence_mask][4] - y_pred[object_presence_mask][4]) ** 2 +

src/grund/utils/math.py
@@ -24,19 +24,25 @@ def xywh2xyxy(bboxes: torch.Tensor) -> torch.Tensor:
 
 def compute_iou(bbox_a_xyxy: torch.Tensor, bbox_b_xyxy: torch.Tensor) -> torch.Tensor:
 
-    inter_xy_min = torch.max(bbox_a_xyxy[:2], bbox_b_xyxy[:2])
-    inter_xy_max = torch.min(bbox_a_xyxy[3:], bbox_b_xyxy[3:])
+    assert bbox_a_xyxy.ndim == 2, f"Expected `bbox_a_xyxy` dimension to be 2, got '{bbox_a_xyxy.ndim}'"
+    assert bbox_b_xyxy.ndim == 2, f"Expected `bbox_b_xyxy` dimension to be 2, got '{bbox_b_xyxy.ndim}'"
 
-    inter_area = torch.max(torch.zeros(2), inter_xy_max - inter_xy_min)
-    inter_area = inter_area[0] * inter_area[1]
+    inter_xy_min = torch.max(bbox_a_xyxy[:, :2], bbox_b_xyxy[:, :2])
+    inter_xy_max = torch.min(bbox_a_xyxy[:, 2:], bbox_b_xyxy[:, 2:])
+    
+    intersections = torch.max(torch.zeros(2), inter_xy_max - inter_xy_min)
+    intersections = intersections[:, 0] * intersections[:, 1]
 
     eps = 1e-6
-    if inter_area < eps:
-        return torch.tensor(0.0, dtype=torch.float32)
 
-    a_area = (bbox_a_xyxy[2] - bbox_a_xyxy[0]) * (bbox_a_xyxy[3] - bbox_a_xyxy[1])
-    b_area = (bbox_b_xyxy[2] - bbox_b_xyxy[0]) * (bbox_b_xyxy[3] - bbox_b_xyxy[1])
+    a_area = (bbox_a_xyxy[:, 2] - bbox_a_xyxy[:, 0]) * (bbox_a_xyxy[:, 3] - bbox_a_xyxy[:, 1])
+    b_area = (bbox_b_xyxy[:, 2] - bbox_b_xyxy[:, 0]) * (bbox_b_xyxy[:, 3] - bbox_b_xyxy[:, 1])
 
-    union = a_area + b_area - inter_area + eps
+    unions = a_area + b_area - intersections + eps
 
-    return inter_area / union
+    iou = intersections / unions
+
+    # Special case with no intersection
+    iou[intersections < eps] = 0.0
+
+    return iou

test/grund/utils/test_math.py
@@ -10,8 +10,8 @@ class TestMathUtils:
     def test__given_valid_bboxes__when_compute_iou__then_ok(self):
 
         # Given
-        bbox_a = torch.tensor([1, 1, 3, 3], dtype=torch.float32)
-        bbox_b = torch.tensor([2, 2, 4, 4], dtype=torch.float32)
+        bbox_a = torch.tensor([[1, 1, 3, 3]], dtype=torch.float32)
+        bbox_b = torch.tensor([[2, 2, 4, 4]], dtype=torch.float32)
 
         # When
         iou = compute_iou(bbox_a, bbox_b)
@@ -19,17 +19,31 @@ class TestMathUtils:
         # Then
         npt.assert_almost_equal(iou.numpy(force=True), 1/7, decimal=7)
 
+    def test__given_multiple_bboxes_against_one__when_compute_iou__then_ok(self):
+
+        # Given
+        bboxes_a = torch.tensor([[1, 1, 3, 3], [1, 2, 3, 4]], dtype=torch.float32)
+        bbox_b = bbox_b = torch.tensor([[2, 2, 4, 4]], dtype=torch.float32)
+
+        # When
+        ious_ab = compute_iou(bboxes_a, bbox_b)
+        ious_ba = compute_iou(bbox_b, bboxes_a)
+
+        # Then
+        npt.assert_allclose(ious_ab.numpy(force=True), np.array([1/7, 1/3], dtype=np.float32), atol=1e-7)
+        npt.assert_allclose(ious_ba.numpy(force=True), np.array([1/7, 1/3], dtype=np.float32), atol=1e-7)
+
     def test__given_non_intersecting_bboxes__when_compute_iou__then_ok(self):
 
         # Given
-        bbox_a = torch.tensor([1, 1, 3, 3], dtype=torch.float32)
-        bbox_b = torch.tensor([4, 4, 5, 5], dtype=torch.float32)
+        bbox_a = torch.tensor([[1, 1, 3, 3]], dtype=torch.float32)
+        bbox_b = torch.tensor([[4, 4, 5, 5]], dtype=torch.float32)
 
         # When
         iou = compute_iou(bbox_a, bbox_b)
 
         # Then
-        npt.assert_almost_equal(iou.numpy(force=True), 0.0, decimal=7)
+        npt.assert_almost_equal(iou.flatten().numpy(force=True), 0.0, decimal=7)
 
     def test__given_valid_bbox__when_xywh2xyxy__then_ok(self):
 
@@ -40,4 +54,4 @@ class TestMathUtils:
         bbox_xyxy = xywh2xyxy(bbox_xywh)
 
         # Then
-        npt.assert_allclose(bbox_xyxy.numpy(force=True), np.array([[1, 1, 3, 3]]))
+        npt.assert_allclose(bbox_xyxy.flatten().numpy(force=True), np.array([1, 1, 3, 3]))